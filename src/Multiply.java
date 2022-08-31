bfc0dd49c5b3e1c8086e2b4adc79ff27d59508e6
benedictELV2002 <2211737@slu.edu.ph>
2022-08-31 14:53:16 +0800

    codewars

--- src/Multiply.java
+++ src/Multiply.java
@@ -1,4 +1,6 @@
 public class Multiply {
+    // 8 kyu
+    //Multiply
     public static Double multiply(Double a, Double b) {
         return a * b;
     }
--- src/Positive.java
+++ src/Positive.java
@@ -1,4 +1,6 @@
 public class Positive {
+    //8 kyu
+    //sum of positives
     public static void main(String[] args){
         System.out.println(sum(new int[]{-1,-2,-3,-4,-5}));
         System.out.println(sum(new int[]{1,2,3,4,5}));
