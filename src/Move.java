d30e1b6b779ac461a0dfc1e2c38e73689c1ccf5c
Bernabe-PC <2211939@slu.edu.ph>
2022-08-31 17:59:45 +0800

    codewars

---
+++ src/Move.java
@@ -0,0 +1,7 @@
+public class Move {
+    // 8 kyu
+    // Grasshopper - Terminal game move function
+    public static int move(int position, int roll) {
+        return position + roll * 2;
+    }
+}
